a283bf328e0d2f13dea9358c12cee7cb32d4564b
Zephaniah <zephaniahko@gmail.com>
2020-10-30 11:03:57 -0700

    imagedatagenerator working, reorganize dirs

---
+++ node/pedestrian.py
@@ -0,0 +1,83 @@
+#! /usr/bin/env/python
+import numpy as np
+import cv2
+from matplotlib import pyplot as plt
+from keras import layers
+from keras import models
+from keras import optimizers
+from keras.utils import plot_model
+from keras import backend
+# from tensorflow import keras
+# from keras.preprocessing.image import image_dataset_from_directory
+from keras.preprocessing.image import ImageDataGenerator
+
+def preprocess_dataset():
+    # return keras.preprocessing.image_dataset_from_directory(
+    #     '../training_pictures/ped_sets',
+    #     labels="inferred",
+    #     label_mode="binary",
+    #     class_names=["ped_N", "ped_Y"],
+    #     color_mode="rgb",
+    #     batch_size= 50,
+    #     image_size=(1280, 720),
+    #     shuffle=False,
+    #     validation_split= 0.2,
+    #     subset="training",
+    # )
+    idg = ImageDataGenerator(
+    featurewise_center=False,
+    samplewise_center=False,
+    featurewise_std_normalization=False,
+    samplewise_std_normalization=False,
+    zca_whitening=False,
+    zca_epsilon=1e-06,
+    rotation_range=0,
+    width_shift_range=0.0,
+    height_shift_range=0.0,
+    brightness_range=None,
+    shear_range=0.0,
+    zoom_range=0.0,
+    channel_shift_range=0.0,
+    fill_mode="nearest",
+    cval=0.0,
+    horizontal_flip=False,
+    vertical_flip=False,
+    rescale=None,
+    preprocessing_function=None,
+    data_format=None,
+    validation_split=0.0,
+    # dtype="int",
+    )
+
+
+    return idg.flow_from_directory(
+    '../training_pictures/ped_sets/',
+    # target_size=(1280, 720),
+    target_size=(720, 1280),
+    color_mode="rgb",
+    classes=['ped_N', 'ped_Y'],
+    class_mode="binary",
+    batch_size=32,
+    shuffle=True,
+    seed=None,
+    save_to_dir=None,
+    save_prefix="",
+    save_format="jpg",
+    follow_links=False,
+    subset=None,
+    interpolation="nearest",
+)
+
+def setupNN():
+   model = models.Sequential()
+#    model.add() 
+
+
+if __name__=="__main__":
+    iterator = preprocess_dataset()
+    print(iterator[0][0][0])
+    x,y = iterator.next()
+    for i in range(0, 1):
+        image = x[1]
+        plt.imshow(image)
+        plt.show()
