f322cf4aa2b251eba6e190229ecb882a1d52e6be
Zephaniah <zephaniahko@gmail.com>
2020-12-01 21:05:20 -0800

    added stop timer publish with small delay

--- node/plate.py
+++ node/plate.py
@@ -203,7 +203,8 @@ class Plate_matcher():
                 self.done_outside = True
                 # outer_lap_pub.publish(True)
             if self.car_num == 8:
-                plate_publisher.publish("funMode,passwd,-1,XR58"
+                rospy.sleep(2)
+                plate_publisher.publish("funMode,passwd,-1,XR58")
     def get_plate(self):
         self.get_template_from_path()
         gray_template = cv2.cvtColor(self.template, cv2.COLOR_BGR2GRAY)
